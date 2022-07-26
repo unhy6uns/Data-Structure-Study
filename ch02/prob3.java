b3e7cebadf6c7cb4e224096ea2888c6084702bb1
unhy6uns <koosh0323@snu.ac.kr>
2022-07-26 11:42:29 +0900

    0726 study

--- ch02/prob3.java
+++ ch02/prob3.java
@@ -1,14 +1,15 @@
 
 public class prob3 {
-    public int count=0;
+    public static int count=0;
     
-    public int fib(int n){
+    public static int fib(int n){
+        count++;
         if (n==1||n==2) return 1;
         else return fib(n-1) + fib(n-2);
-        
     }
 
     public static void main(String[] args){
-
+        System.out.println(fib(5));
+        System.out.println(count);
     }
 }
